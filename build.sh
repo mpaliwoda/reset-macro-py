84d0aef71ab176ef6f3f9d082d7be020b3682f01
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2021-05-22 18:23:11 +0200

    Remove unnecessary linebreaks, use correct package name for Linux

--- build.sh
+++ build.sh
@@ -4,17 +4,20 @@ options=(
     -m nuitka
     --assume-yes-for-downloads
     --standalone main.py
-    --include-data-file=config.ini=config.ini \
-    --include-data-file=logger.conf=logger.conf \
-    --include-data-file=world_counter_1_14.txt=world_counter_1_14.txt \
-    --include-data-file=world_counter_1_16.txt=world_counter_1_16.txt \
+    --include-data-file=config.ini=config.ini
+    --include-data-file=logger.conf=logger.conf
+    --include-data-file=world_counter_1_14.txt=world_counter_1_14.txt
+    --include-data-file=world_counter_1_16.txt=world_counter_1_16.txt 
 )
+
+[ -d "./main.build" ] && rm -r "./main.build"
+[ -d "./main.dist" ] && rm -r "./main.dist"
 
 
 if [ "$(uname)" == "Darwin" ]; then
     options+=(--include-package=pyautogui --include-package=AppKit --include-package=Quartz)
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    options+=(--include-package=pyautogui --include-package=python-xlib)
+    options+=(--include-package=pyautogui --include-package=Xlib)
 fi
 
 
